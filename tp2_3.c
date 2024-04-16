0ecdedae26471ed4bbf48922928dd25c8acd6436
“marijoroldan” <marijoroldan42@gmail.com>
2024-04-15 21:13:25 -0300

    punto3 modi

--- tp2_3.c
+++ tp2_3.c
@@ -4,26 +4,26 @@
 
 int main(int argc, char const *argv[])
 {
-    
+
 #define N 5
 #define M 7
 
-srand(time(NULL));
+    srand(time(NULL));
 
-int i,j;
-int mt[N][M];
+    int i, j;
+    int mt[N][M];
 
-int *pMT;
-pMT = &mt[N][M];
-for(i = 0;i<N; i++)
-{
-for(j = 0;j<M; j++)
-{
-//mt[i][j]=*(pMT+ (i*));
-printf("%d", mt[i][j]);
-}
-printf("\n");
-}
+    int *pMT;
+    pMT = &mt[N][M];
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+            *(pMT + (i * M + j)) = 1 + rand() % 100;
+            printf("%d", *(pMT + (i * M + j)));
+        }
+        printf("\n");
+    }
 
     return 0;
 }
